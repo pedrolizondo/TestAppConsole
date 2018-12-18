98b41d8ceca250148de586dbd78d3f27a1af186e
Pedro Lizondo <plizondo@gruposabra.com>
2018-12-18 11:13:01 -0300

    Add crm.

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,29 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
+            string CRMPath = ConfigurationManager.AppSettings.Get("CRMPath");
+            string UserName = ConfigurationManager.AppSettings.Get("UserName");
+            string Password = ConfigurationManager.AppSettings.Get("Password");
+            double Timeout = Convert.ToDouble(ConfigurationManager.AppSettings.Get("CRMTimeout"));
+
+            Uri oUri = new Uri(CRMPath + "/XRMServices/2011/Organization.svc");
+
+            ClientCredentials clientCredentials = new ClientCredentials();
+            clientCredentials.UserName.UserName = UserName;
+            clientCredentials.UserName.Password = Password;
+
+            var crmInstance = new OrganizationServiceProxy(oUri, null, clientCredentials, null);
+            crmInstance.Timeout = TimeSpan.FromSeconds(Timeout);
+            crmInstance.EnableProxyTypes();
+
+            // Instantiate QueryExpression QEjnbs_location
+            var QEjnbs_location = new QueryExpression("jnbs_location");
+            QEjnbs_location.TopCount = 50;
+
+            // Add columns to QEjnbs_location.ColumnSet
+            QEjnbs_location.ColumnSet.AddColumns("jnbs_name", "jnbs_code", "createdon");
+
+            var response = crmInstance.RetrieveMultiple(QEjnbs_location);
         }
     }
 }
