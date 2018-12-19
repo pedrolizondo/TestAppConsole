e95a6afbfe2cf23d298eec641ada799f93034d70
José Luis Marrone <jmarrone@gruposabra.com>
2018-12-19 12:05:24 -0300

    Update Program.cs
    Minor changes to include pagination into look up example

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -19,6 +19,13 @@ namespace TestConsoleApp
             string Password = ConfigurationManager.AppSettings.Get("Password");
             double Timeout = Convert.ToDouble(ConfigurationManager.AppSettings.Get("CRMTimeout"));
 
+            // The number of records per page to retrieve.
+            int queryCount = 1;
+            // Initialize the page number.
+            int pageNumber = 1;
+            // Initialize the number of records.
+            int recordCount = 0;
+
             Uri oUri = new Uri(CRMPath + "/XRMServices/2011/Organization.svc");
 
             ClientCredentials clientCredentials = new ClientCredentials();
@@ -31,12 +38,34 @@ namespace TestConsoleApp
 
             // Instantiate QueryExpression QEjnbs_location
             var QEjnbs_location = new QueryExpression("jnbs_location");
-            QEjnbs_location.TopCount = 50;
+            //QEjnbs_location.TopCount = 50;
+            QEjnbs_location.PageInfo = new PagingInfo();
+            QEjnbs_location.PageInfo.Count = queryCount;
+            QEjnbs_location.PageInfo.PageNumber = pageNumber;
 
             // Add columns to QEjnbs_location.ColumnSet
             QEjnbs_location.ColumnSet.AddColumns("jnbs_name", "jnbs_code", "createdon");
 
-            var response = crmInstance.RetrieveMultiple(QEjnbs_location);
+            while (true)
+            {
+                var response = crmInstance.RetrieveMultiple(QEjnbs_location);
+
+                if (response.Entities != null)
+                {
+                    Console.WriteLine("Records found", ++recordCount);
+                }
+
+                if (response.MoreRecords)
+                {
+                    QEjnbs_location.PageInfo.PageNumber++;
+                    QEjnbs_location.PageInfo.PagingCookie = response.PagingCookie;
+                }
+                else
+                {
+                    // If no more records are in the result nodes, exit the loop.
+                    break;
+                }
+            }
 
             // Add Options Set for Lead Test.
             var attributeRequest = new RetrieveAttributeRequest
